cea41e6f1581d629c05a6f6de7d20b46f84b056e
lion <lion117@126.com>
2015-11-28 17:29:37 +0800

    add demo into project

---
+++ demo/thrift_exception/main_console.cpp
@@ -0,0 +1,16 @@
+// Console_cpluse_extend.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+// int _tmain(int argc, _TCHAR* argv[])
+// {
+// 
+// 	system("pause");
+// 	return 0;
+// }
+// 
---
+++ demo/thrift_exception_client/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using THRIFT;
+using Thrift.Protocol;
+using Thrift;
+using Thrift.Transport;
+using Thrift.Server;
+using System.Threading;
+
+//@-essential add code line
+
+
+
+namespace thrift_exception_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("thrift client  start");
+            testThrift_rname_fuck();
+
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// ///////////////////////////
+        /// ///////////////
+        /// 
+        /// 
+        /// </summary>
+        static void testThrift_rname_fuck()
+        {
+
+            TTransport i_socket = new TSocket("192.168.15.134", 8081);
+            TProtocol i_protocol = null;
+            RPCNetwork.Client i_client = null;
+            try
+            {
+                i_socket.Open();
+                i_protocol = new TBinaryProtocol(i_socket);
+                i_client = new RPCNetwork.Client(i_protocol);
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("trigger....");
+                try
+                {
+                    Console.WriteLine(i_client.sayHi());
+                }
+                catch (Error ex)
+                {
+                    Console.WriteLine(ex._message);
+                }
+            }
+
+
+
+        }
+    }
+}
